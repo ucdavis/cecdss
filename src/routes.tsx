8cadd0519b9cf62ab116cfa4a520a81c317f2122
Laura Holstege <laholstege@ucdavis.edu>
2019-06-14 11:19:07 -0700

    fix import

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Route } from 'react-router-dom';
 import App from './App';
 import { MapContainer } from './components/Map/MapContainer';
-import ResultsContainer from './components/ResultsContainer';
+import { ResultsContainer } from './components/ResultsContainer';
 
 export const routes = (
   <App>
