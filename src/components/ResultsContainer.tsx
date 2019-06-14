41b4994a23e0808081ba31a5d7e3666e0c23e775
Laura Holstege <laholstege@ucdavis.edu>
2019-06-14 11:11:22 -0700

    results too

--- src/components/ResultsContainer.tsx
+++ src/components/ResultsContainer.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 
-
-export default class ResultsContainer extends React.Component<{}, {}> {
-
-  render() {
-   
-    return (
-      <div>
-       <h1>Results</h1>
-      </div>
-    );
-  }
-}
+export const ResultsContainer = () => {
+  return (
+    <div>
+      <h1>Results</h1>
+    </div>
+  );
+};
