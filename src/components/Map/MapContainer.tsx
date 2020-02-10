99b74fad25dc8b33813a642442d9987d618917e9
Laura Holstege <laholstege@ucdavis.edu>
2020-02-10 10:46:08 -0800

    update token, clean up

--- src/components/Map/MapContainer.tsx
+++ src/components/Map/MapContainer.tsx
@@ -1,10 +1,5 @@
 import React, { createRef, useState, useEffect } from 'react';
-import {
-  Map,
-  TileLayer,
-  Marker,
-  Circle,
-} from 'react-leaflet';
+import { Map, TileLayer, Marker, Circle } from 'react-leaflet';
 import L, { LatLngExpression, LatLngBoundsExpression } from 'leaflet';
 import { MapSidebar } from './MapSidebar';
 import { Inputs } from '../../models/Types';
@@ -17,14 +12,20 @@ interface IProps {
 
 export const MapContainer = (props: IProps) => {
   console.log(props);
-  const [mapState, setMapState] = useState({ lat: 40.032769, lng: -120.430607 });
+  const [mapState, setMapState] = useState({
+    lat: 40.032769,
+    lng: -120.430607
+  });
   const [showSidebar, toggleSidebar] = useState(false);
 
   let mapRef: any = createRef<Map>();
 
-  const bounds: LatLngBoundsExpression = [[43.7, -125.5], [30, -105.5]];
+  const bounds: LatLngBoundsExpression = [
+    [43.7, -125.5],
+    [30, -105.5]
+  ];
   const accessToken =
-    'pk.eyJ1IjoibGF1cmFob2xzdGVnZSIsImEiOiJjazYxM2UwdDEwM2xrM2ZtbGI5cmhtaDdnIn0.BKYruJdLx9bM2ICwhOwdew';
+    'pk.eyJ1IjoibGF1cmFob2xzdGVnZSIsImEiOiJjazZnc3U3c3gybWw5M25xamc2M2RnNTl2In0.Zcg_-cmP110dT_JzIN3QdA';
   const mapboxTiles =
     'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token=' +
     accessToken;
@@ -56,7 +57,7 @@ export const MapContainer = (props: IProps) => {
         bounds={bounds}
       >
         <TileLayer attribution={attribution} url={mapboxTiles} />
-       
+
         {props.inputs.ExampleParameters.radius > 0 && (
           <Circle
             center={position}
