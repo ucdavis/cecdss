9af913dd5bc6f9666aab2207e2f076ccf37a4d35
Laura Holstege <laholstege@ucdavis.edu>
2020-02-25 16:16:26 -0800

    4 digit precison

--- src/components/Shared/util.ts
+++ src/components/Shared/util.ts
@@ -2,5 +2,5 @@ export const formatNumber = (num: number) => {
   if (!num) {
     return '';
   }
-  return num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  return num.toFixed(4).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
