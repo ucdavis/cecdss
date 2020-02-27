c1ac0cbb027c6341a05f7f5259cec6a34575a608
Laura Holstege <laholstege@ucdavis.edu>
2020-02-27 10:28:17 -0800

    better number formatting

--- src/components/Shared/util.ts
+++ src/components/Shared/util.ts
@@ -2,5 +2,9 @@ export const formatNumber = (num: number) => {
   if (!num) {
     return '';
   }
-  return num.toFixed(4).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+  const options = {
+    minimumFractionDigits: 4,
+    maximumFractionDigits: 4
+  };
+  return num.toLocaleString('en', options);
 };
