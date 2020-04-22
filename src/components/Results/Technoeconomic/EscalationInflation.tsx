cb1b7c1464c3890d6547ba4d170d4bd450f321ab
Laura Holstege <laholstege@ucdavis.edu>
2020-04-22 14:49:34 -0700

    Update EscalationInflation.tsx

--- src/components/Results/Technoeconomic/EscalationInflation.tsx
+++ src/components/Results/Technoeconomic/EscalationInflation.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { formatNumber } from '../../Shared/util';
-import { InputModGPO, InputModCHP } from '@ucdavis/tea/out/models/input.model';
+import {
+  InputModGPO,
+  InputModCHP,
+  InputModGP
+} from '@ucdavis/tea/out/models/input.model';
+import { InputModGPClass } from '../../../models/GPClasses';
+import { InputModGPOClass } from '../../../models/GPOClasses';
+import { InputModCHPClass } from '../../../models/CHPClasses';
 
 interface Props {
-  inputs: InputModGPO | InputModCHP;
+  inputs: InputModGPO | InputModCHP | InputModGP;
 }
 
 export const EscalationInflation = (props: Props) => {
@@ -16,10 +23,33 @@ export const EscalationInflation = (props: Props) => {
             <td>General Inflation (%/y)</td>
             <td>{formatNumber(props.inputs.GeneralInflation)}</td>
           </tr>
-          <tr>
-            <td>Esclation--Fuel (%/y)</td>
-            <td>{formatNumber(props.inputs.EscalationFuel)}</td>
-          </tr>
+          {(props.inputs instanceof InputModGPOClass ||
+            props.inputs instanceof InputModCHPClass) && (
+            <tr>
+              <td>Esclation--Fuel (%/y)</td>
+              <td>{formatNumber(props.inputs.EscalationFuel)}</td>
+            </tr>
+          )}
+          {props.inputs instanceof InputModGPClass && (
+            <>
+              <tr>
+                <td>Escalation--Biomass Fuel (%/y)</td>
+                <td>{formatNumber(props.inputs.EscalationBiomassFuel)}</td>
+              </tr>
+              <tr>
+                <td>Escalation--Dual Fuel (%/y)</td>
+                <td>{formatNumber(props.inputs.EscalationDualFuel)}</td>
+              </tr>
+              <tr>
+                <td>Escalation--Heat sales (%/y)</td>
+                <td>{formatNumber(props.inputs.EscalationHeatSales)}</td>
+              </tr>
+              <tr>
+                <td>Escalation--Char/Ash sales (%/y)</td>
+                <td>{formatNumber(props.inputs.EscalationCharSales)}</td>
+              </tr>
+            </>
+          )}
           <tr>
             <td>Escalation for Production Tax Credit</td>
             <td>{formatNumber(props.inputs.EscalationProductionTaxCredit)}</td>
