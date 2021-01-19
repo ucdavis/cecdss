757a4c8caba6742947adaad926a5b81fd978cf7c
Scott Kirkland <srkirkland@gmail.com>
2021-01-19 13:59:06 -0800

    external layer selector

---
+++ src/components/Map/ExternalLayerSelection.tsx
@@ -0,0 +1,50 @@
+import React, { useCallback, useState } from 'react';
+
+interface Props {
+  onChange: (layers: string[]) => void;
+}
+
+const AllLayers: { [key: string]: string } = {
+    'fire': 'Fire Danger',
+    'ice': 'Ice Danger',
+    'water': 'Flood Danger'
+}
+
+export const ExternalLayerSelection = (props: Props) => {
+  const [layers, setLayers] = useState<string[]>([]);
+
+  const handleClick = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      const layer = event.target.value;
+
+      let newLayers;
+      if (event.target.checked) {
+        newLayers = [...layers, layer];
+      } else {
+        newLayers = layers.filter(l => l !== layer);
+      }
+
+      setLayers(newLayers);
+      props.onChange(newLayers);
+    },
+    [props.onChange, layers]
+  );
+  return (
+    <div>
+      <h3>Additional Layers</h3>
+      {Object.keys(AllLayers).map(layer => (
+        <div className='form-check' key={layer}>
+          <input
+            className='form-check-input'
+            type='checkbox'
+            value={layer}
+            checked={layers.includes(layer)}
+            onChange={handleClick}
+            id={layer}
+          />
+          <label className='form-check-label' htmlFor={layer}>{AllLayers[layer]}</label>
+        </div>
+      ))}
+    </div>
+  );
+};
