40a99d5dc2180c869d49f0fe3fcc7c63fdb0c3a9
Cal <cydoval@mac.com>
2021-03-04 15:23:44 -0700

    config thing

--- src/components/Shared/config.ts
+++ src/components/Shared/config.ts
@@ -1,2 +1,2 @@
-export const serviceUrl = 'https://cecdss-backend.azurewebsites.net/';
-// export const serviceUrl = 'localhost:3000';
+export const serviceUrl = 'localhost:3000';
+// export const serviceUrl = 'https://cecdss-backend.azurewebsites.net/';
