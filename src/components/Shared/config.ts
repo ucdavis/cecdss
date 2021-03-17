c58ad2cd37869b8ea21b4bace7212ceb7c526fe0
Bryan M Wong <bmwong@ucdavis.edu>
2021-03-17 11:51:53 -0700

    Added legend as a picture to avoid headaches

--- src/components/Shared/config.ts
+++ src/components/Shared/config.ts
@@ -1,6 +1,6 @@
-export const serviceUrl = 'http://localhost:3000/';
-// export const serviceUrl = 'https://cecdss-backend.azurewebsites.net/';
+// export const serviceUrl = 'http://localhost:3000/';
+export const serviceUrl = 'https://cecdss-backend.azurewebsites.net/';
 
 // Run 20 years worth of data
 // TODO: Eventually we want to run 30 full years
-export const NUM_YEARS_TO_RUN = 20;
+export const NUM_YEARS_TO_RUN = 1;
