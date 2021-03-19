13c232c948f01afa61360e9ac0dd738b4961b476
Scott Kirkland <srkirkland@gmail.com>
2021-03-19 14:29:21 -0700

    starter examples on tea validity

--- src/components/Inputs/validation.ts
+++ src/components/Inputs/validation.ts
@@ -1,7 +1,11 @@
 import * as yup from 'yup';
 import { SchemaOf, ValidationError } from 'yup';
-
-import { FrcsInputs } from '../../models/Types';
+import {
+  InputModGPO,
+  InputModCHP,
+  InputModGP
+} from '@ucdavis/tea/out/models/input.model';
+import { FrcsInputs, TechnoeconomicModels } from '../../models/Types';
 
 // FrcsInputs
 let frcsSchema: SchemaOf<FrcsInputs> = yup.object().shape({
@@ -13,6 +17,38 @@ let frcsSchema: SchemaOf<FrcsInputs> = yup.object().shape({
 export const checkFrcsValidity = async (inputs: FrcsInputs) => {
   try {
     await frcsSchema.validate(inputs);
+  } catch (err) {
+    if (err instanceof ValidationError) {
+      return err.errors;
+    }
+  }
+
+  return [];
+};
+
+// Tea Example
+// TODO: each schema need to be defined as SchemaOf<InputModGPO> or correct type
+// Then each of the properties need to be listed with proper validation rules.
+// Eventually we'll even want to make sure the values are between certain numbers, but for now
+// we'll just make sure that any percentages are between 0 and 100 and leave everything else alone.
+let teaGPOSchema = yup.object().shape({});
+let teaCHPSchema = yup.object().shape({});
+let teaGPSchema = yup.object().shape({});
+
+export const checkTeaValidity = async (
+  model: string,
+  inputs: InputModGPO | InputModCHP | InputModGP
+) => {
+  try {
+    if (model === TechnoeconomicModels.genericPowerOnly) {
+      await teaGPOSchema.validate(inputs);
+    }
+    if (model === TechnoeconomicModels.genericCombinedHeatAndPower) {
+      await teaCHPSchema.validate(inputs);
+    }
+    if (model === TechnoeconomicModels.gasificationPower) {
+      await teaGPSchema.validate(inputs);
+    }
   } catch (err) {
     if (err instanceof ValidationError) {
       return err.errors;
