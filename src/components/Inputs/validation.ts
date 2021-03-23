1edd3c787822485d41fa0ddb6af32b1cc0a33b0a
Bryan M Wong <bmwong@ucdavis.edu>
2021-03-23 15:10:41 -0700

    Added schemas for GP

--- src/components/Inputs/validation.ts
+++ src/components/Inputs/validation.ts
@@ -10,8 +10,8 @@ import { FrcsInputs, TechnoeconomicModels } from '../../models/Types';
 // FrcsInputs
 let frcsSchema: SchemaOf<FrcsInputs> = yup.object().shape({
   system: yup.string().required(),
-  dieselFuelPrice: yup.number().required().positive(),
-  treatmentid: yup.number().required()
+  dieselFuelPrice: yup.number().required().min(0),
+  treatmentid: yup.number().required().min(0)
 });
 
 export const checkFrcsValidity = async (inputs: FrcsInputs) => {
@@ -31,7 +31,66 @@ export const checkFrcsValidity = async (inputs: FrcsInputs) => {
 // Then each of the properties need to be listed with proper validation rules.
 // Eventually we'll even want to make sure the values are between certain numbers, but for now
 // we'll just make sure that any percentages are between 0 and 100 and leave everything else alone.
-let teaGPOSchema = yup.object().shape({});
+let electricalFuelBaseYearSchema = yup.object().shape({
+  NetElectricalCapacity: yup.number().required().min(0),
+  NetStationEfficiency: yup.number().required().min(0),
+  CapacityFactor: yup.number().required().min(0),
+  MoistureContent: yup.number().required().min(0),
+  FuelHeatingValue: yup.number().required().min(0),
+  FuelAshConcentration: yup.number().required().min(0)
+});
+
+let escalationInflationSchema = yup.object().shape({
+  EscalationBiomassFuel: yup.number().required().min(0),
+  EscalationCharSales: yup.number().required().min(0),
+  EscalationDualFuel: yup.number().required().min(0),
+  EscalationHeatSales: yup.number().required().min(0),
+  EscalationOther: yup.number().required().min(0),
+  EscalationProductionTaxCredit: yup.number().required().min(0),
+  GeneralInflation: yup.number().required().min(0)
+});
+
+let expenseBaseYearSchema = yup.object().shape({
+  AshDisposal: yup.number().required().min(0),
+  BiomassFuelCost: yup.number().required().min(0),
+  InsurancePropertyTax: yup.number().required().min(0),
+  LaborCost: yup.number().required().min(0),
+  MaintenanceCost: yup.number().required().min(0),
+  Management: yup.number().required().min(0),
+  OtherOperatingExpenses: yup.number().required().min(0),
+  Utilities: yup.number().required().min(0)
+});
+
+let financingSchema = yup.object().shape({
+  CostOfEquity: yup.number().required().min(0),
+  DebtRatio: yup.number().required().min(0),
+  EconomicLife: yup.number().required().min(0),
+  InterestRateOnDebt: yup.number().required().min(0)
+});
+
+let incomeOtherThanEnergySchema = yup.object().shape({
+  CapacityPayment: yup.number().required().min(0),
+  InterestRateOnDebtReserve: yup.number().required().min(0),
+  SalesPriceForChar: yup.number().required().min(0)
+});
+
+let taxesSchema = yup.object().shape({
+  FederalTaxRate: yup.number().required().min(0),
+  ProductionTaxCredit: yup.number().required().min(0),
+  StateTaxRate: yup.number().required().min(0)
+});
+
+let teaGPOSchema = yup.object().shape({
+  CapitalCost: yup.number().required().min(0),
+  ElectricalFuelBaseYear: electricalFuelBaseYearSchema,
+  EscalationInflation: escalationInflationSchema,
+  ExpensesBaseYear: expenseBaseYearSchema,
+  Financing: financingSchema,
+  IncomeOtherThanEnergy: incomeOtherThanEnergySchema,
+  TaxCreditFrac: yup.array().of(yup.number()),
+  Taxes: taxesSchema
+});
+
 let teaCHPSchema = yup.object().shape({});
 let teaGPSchema = yup.object().shape({});
 
@@ -40,13 +99,13 @@ export const checkTeaValidity = async (
   inputs: InputModGPO | InputModCHP | InputModGP
 ) => {
   try {
-    if (model === TechnoeconomicModels.genericPowerOnly) {
+    if (model === "GPO") {
       await teaGPOSchema.validate(inputs);
     }
-    if (model === TechnoeconomicModels.genericCombinedHeatAndPower) {
+    if (model === "CHP") {
       await teaCHPSchema.validate(inputs);
     }
-    if (model === TechnoeconomicModels.gasificationPower) {
+    if (model === "GP") {
       await teaGPSchema.validate(inputs);
     }
   } catch (err) {
