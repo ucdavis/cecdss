24a90784d95fb59305099c2222093c0dde82fdf5
Scott Kirkland <srkirkland@gmail.com>
2021-05-19 10:59:59 -0700

    try to use the azure appsetting

--- src/components/Shared/config.ts
+++ src/components/Shared/config.ts
@@ -1,10 +1,10 @@
-export const serviceUrl = process.env.SERVICE_URL
-  ? process.env.SERVICE_URL
+export const serviceUrl = process.env.APPSETTING_SERVICE_URL
+  ? process.env.APPSETTING_SERVICE_URL
   : 'http://localhost:3000/';
 // export const serviceUrl = 'https://cecdss-backend.azurewebsites.net/';
 
 // Run 20 years worth of data
 // TODO: Eventually we want to run 30 full years
-export const NUM_YEARS_TO_RUN = process.env.NUM_YEARS_TO_RUN
-  ? process.env.NUM_YEARS_TO_RUN
+export const NUM_YEARS_TO_RUN = process.env.APPSETTING_NUM_YEARS_TO_RUN
+  ? process.env.APPSETTING_NUM_YEARS_TO_RUN
   : 20;
