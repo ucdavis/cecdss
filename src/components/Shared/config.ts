dd0a580fb1afc0628941cf81d8873d031cc9c3ce
Scott Kirkland <srkirkland@gmail.com>
2021-06-04 10:21:44 -0700

    attempt to run against cloud backend

--- src/components/Shared/config.ts
+++ src/components/Shared/config.ts
@@ -1,10 +1,9 @@
-export const serviceUrl = process.env.APPSETTING_SERVICE_URL
-  ? process.env.APPSETTING_SERVICE_URL
-  : 'http://localhost:3000/';
-// export const serviceUrl = 'https://cecdss-backend.azurewebsites.net/';
+const LOCAL_DEVEOPMENT_MODE = false;
+
+export const serviceUrl = LOCAL_DEVEOPMENT_MODE
+  ? 'http://localhost:3000/'
+  : 'https://cecdss-backend.azurewebsites.net/';
 
 // Run 20 years worth of data
 // TODO: Eventually we want to run 30 full years
-export const NUM_YEARS_TO_RUN = process.env.APPSETTING_NUM_YEARS_TO_RUN
-  ? process.env.APPSETTING_NUM_YEARS_TO_RUN
-  : 20;
+export const NUM_YEARS_TO_RUN = LOCAL_DEVEOPMENT_MODE ? 20 : 20;
