8e3e1b3656aa54c3e50eb08cf8b16ff065ff1ebc
Bryan M Wong <bmwong@ucdavis.edu>
2021-09-15 15:27:27 -0700

    Removed console .logs

--- src/components/Results/ResultsTables.tsx
+++ src/components/Results/ResultsTables.tsx
@@ -25,11 +25,6 @@ export const ResultsTable = (props: Props) => {
     x => x.id === props.frcsInputs.treatmentid
   );
   const treatmentName = Treatments[treatmentIndex].name;
-  console.log(
-    `index: ${treatmentIndex}, Treatment: ${JSON.stringify(
-      Treatments[treatmentIndex]
-    )}`
-  );
   const renderTechnicalPerformanceTable = () => {
     return (
       <Table responsive bordered hover>
