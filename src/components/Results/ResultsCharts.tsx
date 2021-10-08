6f1e5397c4c3f812677ec12401dd0b1761ddee30
Kaiyan <kkyli@ucdavis.edu>
2021-10-07 17:20:48 -0700

    add name and change unit

--- src/components/Results/ResultsCharts.tsx
+++ src/components/Results/ResultsCharts.tsx
@@ -57,32 +57,36 @@ export const ResultsCharts = (props: Props) => {
           <Area
             type='monotone'
             dataKey='harvestCosts'
+            name='Harvest Cost'
             stackId='1'
             stroke='#8884d8'
             fill='#8884d8'
-            unit='$/green ton'
+            unit='$/BDT'
           />
           <Area
             type='monotone'
             dataKey='transportationCosts'
+            name='Transport Cost'
             stackId='1'
             stroke='#82ca9d'
             fill='#82ca9d'
-            unit='$/green ton'
+            unit='$/BDT'
           />
           <Area
             type='monotone'
             dataKey='moveInCosts'
+            name='Move-in Cost'
             stackId='1'
             stroke='#ffc658'
             fill='#ffc658'
-            unit='$/green ton'
+            unit='$/BDT'
           />
           <Line
             type='monotone'
             dataKey='total'
+            name='Feedstock Cost'
             stroke='#ff7300'
-            unit='$/green ton'
+            unit='$/BDT'
           />
         </ComposedChart>
         {props.sensitivityResults && <></>}
