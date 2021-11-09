b58ac4950e1d8b9c021cba5eb13637653df16404
Scott Kirkland <srkirkland@gmail.com>
2021-11-09 13:15:48 -0800

    hide broken power plant layer

--- src/components/Map/ExternalLayerSelection.tsx
+++ src/components/Map/ExternalLayerSelection.tsx
@@ -12,7 +12,7 @@ const AllLayers: { [key: string]: string } = {
   fire: 'Fire Hazard Zones',
   transmission: 'Transmission Lines',
   substation: 'Substations',
-  plant: 'Power plants',
+  // plant: 'Power plants',
   ownership: 'Ownership',
   county: 'County Lines',
   urbanCities: 'Urban City Limits',
