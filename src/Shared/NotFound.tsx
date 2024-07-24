cbaa150387cbdfa44c699256fa64737b6150b95b
aunshx <aunsh.sb@gmail.com>
2024-07-23 20:26:05 -0700

    Added tailwind

--- src/Shared/NotFound.tsx
+++ src/Shared/NotFound.tsx
@@ -1,8 +1,12 @@
+import { faRobot } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='w-screen h-screen bg-brand flex items-center justify-center'>
+        <FontAwesomeIcon icon={faRobot} />
+    </div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#395442'
+      }
+    },
+  },
+  plugins: [],
+}
